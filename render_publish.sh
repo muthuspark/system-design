30cba3cdad86bb1df15793034b1736837d9955f5
muthuspark <muthukrishnan.t@hotmail.com>
2025-01-30 06:58:04 +0530

    word fix 2025-01-30

--- render_publish.sh
+++ render_publish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-quarto render
+# quarto render
 cp CNAME docs/CNAME
 git add .
 current_date=$(date +"%Y-%m-%d")
-git commit -m "release $current_date"
-# git commit -m "word fix $current_date"
+# git commit -m "release $current_date"
+git commit -m "word fix $current_date"
 git push
